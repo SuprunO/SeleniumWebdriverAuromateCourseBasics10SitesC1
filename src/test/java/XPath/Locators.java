1ae6d39dacdd41d6bf2932de50ade41317709dd7
SuprunO <oleksiisuprun@gmail.com>
2017-01-10 22:52:31 +0200

    Selenium_Webdriver_Auromate_Course_Basics_C1 10.01.2017

---
+++ src/test/java/XPath/Locators.java
@@ -0,0 +1,18 @@
+package XPath;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by alex on 10.01.2017.
+ */
+public class Locators {
+
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://techno-geek.co.uk/callmycab");
+        driver.manage().window().maximize();
+        driver.findElement(By.xpath(".//*[@id='navbar']/ul/li[1]/a/nav/span[2]"));
+    }
+}
